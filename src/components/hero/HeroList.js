19b407726d5b7f8dda93687b4ca4064d29e87eec
Jose Candia <josecandiap@gmail.com>
2022-03-31 23:41:00 -0300

    creacion de cards

---
+++ src/components/hero/HeroList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { getHeroByPublisher } from '../../selectors/getHeroByPublisher'
+import { HeroCard } from './HeroCard';
+
+export const HeroList = ({ publisher }) => {
+  
+    const heroes = getHeroByPublisher( publisher );
+
+    return (
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
+        {
+            heroes.map( hero => (
+                <HeroCard 
+                    key={ hero.id }
+                    { ...hero }
+                />
+            ))
+        }
+    </div>
+  )
+}
--- src/routes/DashboardRoutes.js
+++ src/routes/DashboardRoutes.js
@@ -10,15 +10,18 @@ export const DashboardRoutes = () => {
   return (
     <>
         <Navbar />
-        <Routes>
-            <Route path="marvel" element={<MarvelScreen />} />
-            <Route path="dc" element={<DcScreen />} />
-            <Route path="hero" element={<HeroScreen />} />
+        
+        <div className="container">
+          <Routes>
+              <Route path="marvel" element={<MarvelScreen />} />
+              <Route path="dc" element={<DcScreen />} />
+              <Route path="hero" element={<HeroScreen />} />
 
-            <Route path="search" element={<SearchScreen />} />
-            
-            <Route path="/" element={<MarvelScreen />} />
-        </Routes>
+              <Route path="search" element={<SearchScreen />} />
+              
+              <Route path="/" element={<MarvelScreen />} />
+          </Routes>
+        </div>
     </>
   )
 }
